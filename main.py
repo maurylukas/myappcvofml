b33fd2cf3de79f2c6a0c9eac63af8b94d9636ce2
maurylukas <maurylukas@yahoo.com>
2022-02-02 17:31:55 -0300

    after Kivy
    Kivy installed and imported

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+# start of imports
+from kivy.app import App
+from kivy.lang import Builder
+# end of imports
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# generating application
+GUI = Builder.load_file("main.kv")
 
+class MainApp (App):
+    def build(self):
+        return GUI
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
+# running application
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    MainApp ().run()
