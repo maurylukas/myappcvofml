25efc7ad8135bae81dd91e45710cb1291ed7a563
maurylukas <97759783+maurylukas@users.noreply.github.com>
2022-02-03 21:34:48 -0300

    Screens added
    Added some screens and functions to alternate among them.

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 # start of imports
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
+from kivy.core.window import Window
 # end of imports
+
+# generating screens
+class Home (Screen):
+    pass
+class Resume (Screen):
+    pass
+class Projects (Screen):
+    pass
 
 # generating application
 GUI = Builder.load_file("main.kv")
@@ -9,6 +19,24 @@ GUI = Builder.load_file("main.kv")
 class MainApp (App):
     def build(self):
         return GUI
+
+    def on_start(self):
+        Window.bind (on_keyboard = self.voltar)
+
+    def voltar (self, window, key, *args):
+        if key == 27:
+            if self.root.ids ["manager"].current_screen.name == "home":
+                return False
+            elif self.root.ids ["manager"].current_screen.name == "resume" or "projects":
+                self.root.ids ["manager"].current = "home"
+                return True
+            else:
+                self.root.ids ["manager"].current = "projects"
+                return True
+
+    def alternar (self, id):
+        self.root.ids ["manager"].current = id
+
 
 # running application
 if __name__ == '__main__':
